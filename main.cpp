885e78e7eaffceda9a6f20924d849ab16adc2c1a
BaiQiang <1075707996@qq.com>
2018-08-19 23:42:48 +0800

    add screenshot

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "gl_core.h"
+#include "screenshot.h"
 
 static void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
+    } else if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+        screenshot(window, "screenshot.tga");
+    }
 }
 
 int main() {
---
+++ screenshot.h
@@ -0,0 +1,17 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef SCREENSHOT_H
+#define SCREENSHOT_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void screenshot(GLFWwindow *window, const char *path);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
